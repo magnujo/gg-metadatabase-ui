32c04c04e20d2bf5fe56583095d5f2e3714b5541
Magnus Johannsen <glj523@ku.dk>
2024-01-22 09:10:46 +0100

    made parsers.py for parse functions

--- scripts/ETLFunctions.py
+++ scripts/ETLFunctions.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import re
 import locale
+from utils.parsers import parse_dates, parse_floats
+
 admin_email = 'magnus.johannsen@sund.ku.dk'
 
 def clean_up(tsv_file_path, database_table_name, date_format, decimal_point, 
@@ -146,78 +148,3 @@ def parse_archive_sample(file_path, date_format, decimal_point, thousands_sepera
 
     return sheet
     
-def parse_dates(sheet, date_columns, date_format):
-    if date_format == 'ymd':
-        for ele in date_columns:
-                sheet[ele] = pd.to_datetime(sheet[ele], format='ISO8601')
-                sheet[ele] = sheet[ele].astype('datetime64[ns]')
-    elif date_format == 'dmy':
-        for ele in date_columns:
-                sheet[ele] = pd.to_datetime(sheet[ele], format='%d-%m-%Y %H:%M:%S')
-                sheet[ele] = sheet[ele].astype('datetime64[ns]')
-    else: 
-        raise Exception('No date format chosen, try again.')
-    return sheet
-
-#TODO: What if not_relevant?
-# Converts to float and throws error if string is not a float (for example if it contains thousands seperators)
-def parse_floats(sheet, float_columns, decimal_point, thousands_seperator):
-    
-    for ele in float_columns:
-        if ele in sheet.columns:
-        
-            # Checks for inconsistencies in data and user input
-            match (decimal_point, thousands_seperator):
-            
-                case ("not_relevant", ","):
-                    bad_rows = sheet[ele].apply(str).str.contains(".", regex=False)
-                    if bad_rows.any():
-                        raise Exception(f"Found . (period) in numeric data in the following rows, \
-                                        but not in user input: \n \n {list(sheet[bad_rows].index + 2)}")
-                    else:
-                        sheet[ele] = sheet[ele].str.replace(thousands_seperator, "", regex=False)
-                        
-                case (",", "not_relevant"):
-                    bad_rows = sheet[ele].apply(str).str.contains(".", regex=False)
-                    if bad_rows.any():
-                        raise Exception(f"Found . (period) in numeric data in the following rows, \
-                                        but not in user input: \n \n {list(sheet[bad_rows].index + 2)}")
-                    else:
-                        sheet[ele] = sheet[ele].str.replace(decimal_point, ".", regex=False)
-                        
-                case (".", "not_relevant"):
-                    bad_rows = sheet[ele].apply(str).str.contains(",", regex=False)
-                    if bad_rows.any():
-                        raise Exception(f"Found , (comma) in numeric data in the following rows, \
-                                        but not in user input: \n \n {list(sheet[bad_rows].index + 2)}")
-                    
-                case ("not_relevant", "."):
-                    bad_rows = sheet[ele].apply(str).str.contains(",", regex=False)
-                    if bad_rows.any():
-                        raise Exception(f"Found , (comma) in numeric data in the following rows, \
-                                        but not in user input: \n \n {list(sheet[bad_rows].index + 2)}")
-                    else:
-                        sheet[ele] = sheet[ele].str.replace(thousands_seperator, "")
-
-                case ("not_relevant", "not_relevant"):
-                    bad_rows = sheet[ele].apply(str).str.contains("\.|,", regex=True)
-                    if bad_rows.any():
-                        raise Exception(f"Found , (comma) or . (period) in numeric data in the following rows, \
-                                        but not in user input: \n \n {list(sheet[bad_rows].index + 2)}")
-                        
-                case (",", "."):
-                    sheet[ele] = sheet[ele].str.replace(thousands_seperator, "", regex=False)
-                    sheet[ele] = sheet[ele].str.replace(decimal_point, ".", regex=False)
-                        
-                case (".", ","):
-                    sheet[ele] = sheet[ele].str.replace(thousands_seperator, "", regex=False)
-                
-                case _:
-                    raise Exception("case _ reached in parse floats. Contact database admin.")
-                        
-            sheet[ele] = sheet[ele].astype(float)   
-            
-        else:
-            raise Exception(f"Did not find expected float column {ele} in input. Contact admin at {admin_email}")
-
-    return sheet
