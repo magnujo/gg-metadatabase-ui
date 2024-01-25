109d42ae5c8ed56678526ff99be0676cad689094
Magnus Johannsen <glj523@ku.dk>
2024-01-25 13:06:56 +0100

    issue6

---
+++ utils/archive_sample_sheet_parser.py
@@ -0,0 +1,75 @@
+from constants import DATABASE_CONFIG, ADMIN_EMAILS, ENGINE
+import pandas as pd
+from utils.parsers import parse_dates, parse_numerics
+import numpy as np
+
+def parse(file_path, date_format, decimal_point, thousands_seperator):
+    
+    
+    dtypes = {'ArchiveSampleID': str,
+                'PositionInRack': str,
+                'RackName': str,
+                'RackID': str,
+                'BulkSampleID': str,
+                'DepthSampledCalTape': float,
+                'DepthOrderedCalTape': str,
+                'OrganicContent': str,
+                'SurfaceExposed': str,
+                'RemarksArchiveSampling': str,
+                'SampledBy1': str,
+                'SampledBy2': str,
+                'SamplingDate': str,
+                'Submitter': str,
+                'SubmissionDate': str,
+                'NotesSubmitter': str}
+
+    # sheet = pd.read_csv(file_path, sep='\t', encoding='utf_16', dtype=dtypes, thousands='.', decimal=',')
+    sheet = pd.read_csv(file_path, sep='\t', encoding='utf_16', dtype=str)
+    
+    expected_columns = pd.read_sql(sql=f"SELECT * from {DATABASE_CONFIG['schema_name']}.archive_sample", con=ENGINE).columns
+    
+    expected_columns = expected_columns[:-3] 
+    expected_columns2 = expected_columns.copy().drop('SampledBy2')
+    
+    # TODO: Make unit test with mock data.
+    assert list(expected_columns) == list(sheet.columns) or list(expected_columns2) == list(sheet.columns), ("Column names and/or positions not as expected")
+
+    # Converts column names, if they are changes to KU ID format.
+    if 'SampledBy' in sheet.columns:
+        sheet = sheet.rename(columns={'SampledBy': 'SampledBy1'})
+        sheet['SampledBy2'] = np.nan
+        sheet.insert(sheet.columns.get_loc('SampledBy1')+1, 'SampledBy2', sheet.pop('SampledBy2'))
+    elif 'SampleBy2' not in sheet.columns:
+        #sheet = sheet.rename(columns={'SampledBy1': 'SampledBy'})
+        sheet['SampledBy2'] = np.nan
+        sheet.insert(sheet.columns.get_loc('SampledBy1')+1, 'SampledBy2', sheet.pop('SampledBy2'))
+    if 'Submitter(KU-ID)' in sheet.columns:
+        sheet = sheet.rename(columns={'Submitter(KU-ID)': 'Submitter'})
+    
+    # TODO: Delete after deployment and ask make uploader responsible.
+    sheet = sheet.dropna(axis='index', how='all')
+    
+    # For testing that the drops are made correctly:
+    l1 = len(sheet[sheet['ArchiveSampleID'].notnull()])
+
+    # Drop all rows that does not contain ArchiveSampleID
+    if 'ArchiveSampleID' in sheet.columns:
+        print("dropping")
+        # TODO: Remove dropna before deployment. Make users responsible for input to db.
+        sheet = sheet.dropna(axis='index', how='all', subset=['ArchiveSampleID'])
+    else:
+        raise Exception ("Upload failed. Expected column 'ArchiveSampleID' not found. Are you sure you uploaded the correct spreadsheet?")
+    assert l1 == len(sheet), "Dropna test failed"
+    
+    # Parse dates, throws error if formatting is wrong in the sheet
+    date_columns = ['SubmissionDate', 'SamplingDate']
+    sheet = parse_dates(sheet, date_columns=date_columns, date_format=date_format)
+
+    # Convert float cols to float. Throws error if not a float.
+    # sheet['DepthSampledCalTape'] = sheet['DepthSampledCalTape'].astype(float)
+    
+    float_cols = ['DepthSampledCalTape']
+    sheet = parse_numerics(sheet, float_cols, decimal_point, thousands_seperator)
+        
+
+    return sheet
---
+++ utils/robot_sample_sheet_parser.py
@@ -0,0 +1,52 @@
+from utils import parsers
+import constants
+import pandas as pd
+from constants import DATABASE_CONFIG, ADMIN_EMAILS, ENGINE
+import pandas as pd
+from utils.parsers import parse_dates, parse_numerics
+
+def parse(file_path, date_format, database_table_name, decimal_point, thousands_seperator):
+    
+    numeric_columns = ['Mass']
+    date_columns = ['SamplingDate', 'SubmissionDate']
+    
+    if not decimal_point == 'not_relevant':
+        raise Exception(f"Did not expect decimal numbers. Please contact contact {ADMIN_EMAILS} if you think this is a mistake.")
+    
+    # read sheet
+    sheet = pd.read_csv(file_path, sep='\t', encoding='utf_16', dtype=str)
+    
+ # TODO: Delete after deployment and ask make uploader responsible.
+    sheet = sheet.dropna(axis='index', how='all')
+    sheet = sheet.drop(columns=sheet.columns[sheet.columns.str.contains('^Unnamed')])
+
+    # check for expected cols
+    expected_columns = pd.read_sql(sql=f"SELECT * from {constants.DATABASE_CONFIG['schema_name']}.{database_table_name}", con=constants.ENGINE).columns
+    
+    expected_columns = expected_columns[:-3] 
+    
+    print(sheet.columns)
+    print(expected_columns)
+    # TODO: Make unit test with mock data.
+    assert list(expected_columns) == list(sheet.columns), ("Column names and/or positions not as expected")
+
+    num_of_not_null_rows = len(sheet[sheet["SubSampleID"].notnull()]) # For drop testing
+
+    # TODO: Delete after deployment and ask make uploader responsible.
+    if 'SubSampleID' in sheet.columns:
+        sheet = sheet.dropna(axis='index', how='all', subset=["SubSampleID"])
+    else:
+        raise Exception ("Upload failed. Expected column 'Robot TUBE barcode' not found. Are you sure you uploaded the correct spreadsheet?")
+    
+    # Drop test:
+    if len(sheet) != num_of_not_null_rows:
+        raise Exception(f'Error dropping null values. Contact {constants.ADMIN_EMAILS} for help.')
+    
+    # Parse dates, throws error if formatting is wrong in the sheet
+    sheet = parse_dates(sheet, date_columns=date_columns, date_format=date_format)
+            
+    sheet = parse_numerics(sheet, numeric_columns, decimal_point, thousands_seperator)
+    
+    return sheet
+        
+        
