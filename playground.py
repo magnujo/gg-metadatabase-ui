1eb798415a53752853f8812e3d0a72db5593cce5
Magnus Johannsen <glj523@ku.dk>
2024-03-01 14:59:19 +0100

    ...

--- playground.py
+++ playground.py
@@ -1,9 +1,12 @@
-def match_data(data: str, has_dates: bool) -> str:
-    match (data, has_dates):
-        case ("My data doesn't contain dates", True):
-            print("Hej")
-        case (_, False):
-            print("He2")
 
-# Example usage:
-match_data("My data doesn't contain dates", False)
+import pandas as pd
+import json
+# Read the Excel file into a DataFrame
+df = pd.read_excel(r'C:\Users\glj523\Documents\github repoes\upload_web_app\static\example_sheets_online\Field sampling (internal).xlsx')
+df  = df.drop(df.columns[0], axis=1)
+# Convert the DataFrame into a dictionary
+excel_dict = df.to_dict()
+
+
+with open("output.json", "w") as json_file:
+    json.dump(excel_dict, json_file, indent=4)
