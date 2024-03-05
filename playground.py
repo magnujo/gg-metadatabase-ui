5004505df8f4dbf54913ab0ff53376fe581b9fe1
Magnus Johannsen <glj523@ku.dk>
2024-03-05 16:47:06 +0100

    generate_sheet_from_table works

--- playground.py
+++ playground.py
@@ -1,11 +1,7 @@
+from scripts import generate_sheet_from_table
+import constants
 
-import pandas as pd
-import json
-# Read the Excel file into a DataFrame
-df = pd.read_excel(r'C:\Users\glj523\Documents\github repoes\upload_web_app\static\example_sheets_online\Field sampling (internal).xlsx')
-df  = df.drop(df.columns[0], axis=1)
-# Convert the DataFrame into a dictionary
-excel_dict = df.to_dict()
-
-with open("output.json", "w") as json_file:
-    json.dump(excel_dict, json_file, indent=4)
+df = generate_sheet_from_table.query(database_config=constants.DATABASE_CONFIG_2, 
+                                          database_name='aedna_metadata',
+                                     table_name='field_sample_internal',
+                                     schema_name='test')
--- scripts/generate_sheet_from_table.py
+++ scripts/generate_sheet_from_table.py
@@ -1,9 +1,10 @@
-from sqlalchemy import create_engine
+import pandas as pd
+import ast
+import numpy as np
 import sys, os
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
 import constants
-from constants import DATABASE_CONFIG, DATABASE_CONFIG_2
 import pandas as pd
 import psycopg2
 import os, sys
@@ -12,6 +13,10 @@ sys.path.append(parent_dir)
 import constants
 import psycopg2
 import pandas as pd
+
+'''
+Generates a sheet with table information about column name, datatype nua
+'''
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = os.path.join('static', 'auto_sheets')
@@ -29,53 +34,70 @@ data_types = {'integer': None,
               'timestamp without time zone': None, 
               'double precision': None}
 
+def si(value):
+    if value is not None:
+        return ast.literal_eval(value)
+    else:
+        return None
 
-def generate_excel_from_table(output_file, database_config, table_name, schema_name):
+
+def generate_excel_from_table(output_file, database_config, table_name, database_name, schema_name):
     '''
     Generates spreaddsheet with a tables column names and corresponding data types and comments.
     '''
-    
     # Connect to your PostgreSQL database
     conn = psycopg2.connect(**database_config)    
 
     # SQL query to retrieve column comments and data types
     query = f"""
-    SELECT 
-        column_name,
-        udt_name as data_type,
-        col_description(pg_attribute.attrelid, pg_attribute.attnum) as column_comment
-    FROM 
-        information_schema.columns
-    JOIN 
-        pg_attribute ON pg_attribute.attname = information_schema.columns.column_name
-    JOIN 
-        pg_class ON pg_class.oid = pg_attribute.attrelid
-    LEFT JOIN 
-        pg_namespace ON pg_namespace.oid = pg_class.relnamespace
-    WHERE 
-        table_name = '{table_name}'
-        AND pg_namespace.nspname = '{schema_name}'
-        and table_catalog = '{database_config['dbname']}'
-        and relname = '{table_name}';
+select
+    c.column_name,
+    c.data_type,
+    c.is_nullable,
+    pgd.description
+from pg_catalog.pg_statio_all_tables as st
+inner join pg_catalog.pg_description pgd on (
+    pgd.objoid = st.relid
+)
+full outer join information_schema.columns c on (
+    pgd.objsubid   = c.ordinal_position and
+    c.table_schema = st.schemaname and
+    c.table_name   = st.relname
+) where c.table_catalog = '{database_name}' and c.table_schema = '{schema_name}' and c.table_name = '{table_name}';
     """
-
+    
     # Execute the query and fetch the results
     cursor = conn.cursor()
     cursor.execute(query)
     rows = cursor.fetchall()
 
     # Create a DataFrame from the fetched data
-    df = pd.DataFrame(rows, columns=['Column Name', 'Data Type', 'Description'])
+    df = pd.DataFrame(rows, columns=['Column Name', 'Data Type', 'Is Nullable', 'Description'])
     # auto_generated_cols_in_df = [col for col in constants.auto_generated_columns if col in df.columns]
     df = df[~df['Column Name'].isin(constants.auto_generated_columns)]
 
 
     # Export the DataFrame to an Excel file
-    df.to_excel(output_file, index=False)
+    
 
     # Close the cursor and connection
     cursor.close()
     conn.close()
+
+    df['Description'] = df['Description'].str.replace("nan", 'None')
+
+    df['Description'] = df['Description'].apply(si)
+
+    expanded_df = pd.json_normalize(df['Description'])
+    result_df = pd.concat([df, expanded_df], axis=1)
+    result_df = result_df.fillna(np.nan)
+    result_df = result_df.dropna(axis='rows', how='all')
+    result_df = result_df.drop(columns=['Description'])
+    result_df.to_excel(output_file, index=False)
+
+generate_excel_from_table(output_file='test.xlsx', database_config=constants.DATABASE_CONFIG_2, 
+                          table_name='field_sample_internal', schema_name='test',
+                          database_name='aedna_metadata')
 
 def generate_excels_from_schema(output_folder, schema, database_config):
     '''
@@ -109,4 +131,3 @@ def generate_excels_from_schema(output_folder, schema, database_config):
     cur.close()
     conn.close()   
     
-generate_excel_from_table(output_file="test.xlsx", database_config=constants.DATABASE_CONFIG_2, table_name='field_sample_internal', schema_name='test')
