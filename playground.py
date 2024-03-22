cf7e92ebbeebff415ac41995bfe8a0e606104a98
Magnus Johannsen <magnus.johannsen@sund.ku.dk>
2024-03-22 15:38:44 +0100

    upload_uuid generator added and integrity test now matches column positions

--- playground.py
+++ playground.py
@@ -1,11 +1,19 @@
-from utils import queries
-import constants
-p = queries.get_possible_datatypes('date')
-print(p)
+import pandas as pd
 
-SELECT as2."FieldSampleID", as2."ArchiveSampleID", ers."Robot TUBE barcode", eas."eDNA ID", eas."Library ID", eas."Tube Tag Submitted to SeqC",
-as2."DepthSampledCalTape", csw."Country", csw."Lat", csw."Lon", csw."Date collected"
-FROM super_simple_db.edna_archive_sample as2 
-join super_simple_db.edna_robot_sample ers ON as2."ArchiveSampleID" = ers."Archive TUBE barcode"
-left JOIN super_simple_db.edna_wetlab_report eas ON ers."Robot TUBE barcode" = eas."LVL Tube Barcode"
-left join super_simple_db.cgg_sediment_water csw on as2."FieldSampleID" = csw."CGG ID"
+# Example DataFrames
+df1 = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6], 'D': [7, 8, 9]})
+df2 = pd.DataFrame({'B': [10, 11, 12], 'A': [13, 14, 15], 'C': [16, 17, 18]})
+
+print(len(df1.columns) == len(df2.columns))
+
+print(df1.columns.sort_values()==df2.columns.sort_values())
+
+# Reorder columns of df1 to match the column positions of df2
+
+try:
+    df1 = df1[df2.columns]
+except KeyError as e:
+    raise Exception(f"Columns in database table does match number of columns in upload file: {e.args[0]}")
+
+print("DataFrame 1:")
+print(df1)
