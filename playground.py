019f491cb800928cdef8596b6b312cd1d35af275
Magnus Johannsen <magnus.johannsen@sund.ku.dk>
2024-08-07 11:45:01 +0200

    rename_db_table, rename_db_schema and rename_template column in renamer.py works

--- playground.py
+++ playground.py
@@ -1,30 +1,3 @@
-class BaseClass:
-    def __init__(self, name):
-        self.__name = name
-
-    def __str__(self):
-        return self.__name
-
-class ClassA(BaseClass):
-    def __init__(self):
-        super().__init__("ClassA")
-
-class ClassB(BaseClass):
-    def __init__(self):
-        super().__init__("ClassB")
-
-class ClassC(BaseClass):
-    def __init__(self):
-        super().__init__("ClassC")
-
-# Creating instances of the classes
-a = ClassA()
-b = ClassB()
-c = ClassC()
-
-# Printing the instances
-print(a)  # Output: ClassA
-print(b)  # Output: ClassB
-print(c)  # Output: ClassC
+from db_names import db_names
 
 
--- renamer.py
+++ renamer.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from utils import queries
 from constants.misc_constants import PSYCON_CONFIG, PATH_TO_MOUNT
 from db_names import db_names, name_maps, get_column_name, get_schema_name, get_table_name
@@ -13,24 +15,25 @@ db_config["user"] = "glj523"
 db_config["password"] = "Wtcantfw36c!"
 
 
-    
 connection = psycopg2.connect(**db_config)
 
 import json
 
+# TODO: Only allow one rename file to be not empty
+# TODO: Dont allow rename of anything inside name_maps
+#TODO: Make queries safe from sql injection
+# TODO: If rename file is not empty: ask user if they really want to rename
+    # TODO: If rename file is empty: error
 
-# TODO:
-def rename_sheet_column(): 
-    # rename template
-    # rename column_name_sheet in name_maps.column names
-    pass
 
-
-def rename_db_column():
+# TODO: Implement this:
+def rename_template_column():
+    # TODO: Send email notifying people of renaming.
     
-    file_path = os.path.join(PATH_TO_MOUNT, "SUN-GI-metadb-test", "renamer", "db_renamer.json")
+    file_path = os.path.join(PATH_TO_MOUNT, "SUN-GI-metadb-test", "renamer", "template_column_renamer.json")
     
-    # If rename file is not empty: ask user if they really want to rename
+    # TODO: If rename file is not empty: ask user if they really want to rename
+    # TODO: If rename file is empty: error
     
     
     # Load renamer file
@@ -40,7 +43,206 @@ def rename_db_column():
     full_query = ""
     
     for key in rename_file:
-        id = str(key)
+        id = int(key) #  TODO: Int?
+        new_name = rename_file[key]
+        
+        nm = name_maps()
+        table_names = name_maps.table_names()
+        column_names = name_maps.column_names()
+        schema_names = name_maps.schema_names()
+        
+        q1 = f'''
+        select "{schema_names.schema_name}", "{table_names.sheet_template_name}" 
+        from "{name_maps()}"."{column_names}" cn 
+        join "{nm}"."{table_names}" tn on cn."{column_names.table_id}" = tn."{table_names.table_id}" 
+        join "{nm}"."{schema_names}" sn on sn."{schema_names.schema_id}" = tn."{table_names.schema_id}"
+        where {column_names.column_id} = '{id}';
+        '''
+                
+        res = queries.execute_query(q1, connection)
+        
+        if len(res) != 1 or len(res[0]) != 2:
+            raise Exception("res not the expected size")
+        
+        schema_name, template_name = res[0]
+        
+    
+        column_names = name_maps.column_names()
+        
+        #TODO: Make queries safe from sql injection
+        
+        # Rename column in map
+        mapper_update_q = f'''
+        UPDATE "{name_maps()}"."{column_names}"
+        SET "{column_names.column_name_sheet}"='{new_name}'
+        WHERE "{column_names.column_id}"='{id}';
+        '''
+        
+        full_query = full_query + mapper_update_q
+        
+        
+        # Rename template column
+        old_name = get_column_name(id, template=True)
+        
+        # Load the Excel file
+        template_file_path = os.path.join(PATH_TO_MOUNT, "SUN-GI-metadb-test", "standard_spreadsheet_templates", template_name)
+        df = pd.read_excel(template_file_path)
+
+        # Rename the column
+        df = df.rename(columns={old_name: new_name}, errors='raise')
+
+        # Save the DataFrame back to Excel
+        df.to_excel(template_file_path, index=False)
+
+
+        full_query = full_query
+
+    cols_before = {key: get_column_name(int(key), template=True) for key in rename_file}
+    queries.execute_query(full_query, connection)
+    cols_after = {key: get_column_name(int(key), template=True) for key in rename_file}
+        
+    # TODO: Check that names in db are the same as in template?
+    
+    errors = []
+    for key in rename_file:
+        if cols_before[key] == cols_after[key]:
+            errors.append(key)
+    
+    if len(errors) != 0:
+        raise Exception(f"The following columns where not renamed correctly: {errors}")
+
+
+def rename_db_schema():
+    file_path = os.path.join(PATH_TO_MOUNT, "SUN-GI-metadb-test", "renamer", "db_schema_renamer.json")
+    
+    # Load renamer file
+    with open(file_path, 'r') as file:
+        rename_file = json.load(file)
+        
+    full_query = ""
+    
+    for key in rename_file:
+        id = int(key)
+        new_name = rename_file[key]
+        
+        nm = name_maps()
+        schema_names = name_maps.schema_names()
+                    
+        # Rename table_name in map
+        mapper_update_q = f'''
+        UPDATE "{name_maps()}"."{schema_names}"
+        SET "{schema_names.schema_name}"='{new_name}'
+        WHERE "{schema_names.schema_id}"='{id}';
+        '''
+        
+        full_query = full_query + mapper_update_q
+        
+        schema_name = get_schema_name(id)
+        
+        table_update_q = f'''
+        ALTER SCHEMA "{schema_name}"
+        RENAME TO "{new_name}";
+        '''
+        
+        full_query = full_query + table_update_q
+    
+    print(full_query)
+    
+    schemas_before = {key: get_schema_name(int(key)) for key in rename_file}
+    queries.execute_query(full_query, connection)
+    schemas_after = {key: get_table_name(int(key)) for key in rename_file}
+    
+    errors = []
+    for key in rename_file:
+        if schemas_before[key] == schemas_after[key]:
+            errors.append(key)
+    
+    if len(errors) != 0:
+        raise Exception(f"The following columns where not renamed correctly: {errors}")
+
+
+def rename_db_tables():
+    file_path = os.path.join(PATH_TO_MOUNT, "SUN-GI-metadb-test", "renamer", "db_table_renamer.json")
+    
+    # TODO: If rename file is not empty: ask user if they really want to rename
+    # TODO: If rename file is empty: error
+    
+    # Load renamer file
+    with open(file_path, 'r') as file:
+        rename_file = json.load(file)
+        
+    full_query = ""
+    
+    for key in rename_file:
+        id = int(key)
+        new_name = rename_file[key]
+        
+        nm = name_maps()
+        table_names = name_maps.table_names()
+        schema_names = name_maps.schema_names()
+        
+        q1 = f'''
+        select "{schema_names.schema_name}", {table_names.table_name}  
+        from "{nm}"."{table_names}" tn 
+        join "{nm}"."{schema_names}" sn on sn."{schema_names.schema_id}" = tn."{table_names.schema_id}"
+        where {table_names.table_id} = '{id}';
+        '''
+         
+        res = queries.execute_query(q1, connection)
+        
+        if len(res) != 1 or len(res[0]) != 2:
+            raise Exception("res not the expected size")
+        
+        schema_name, table_name = res[0]
+            
+        #TODO: Make queries safe from sql injection
+        
+        # Rename table_name in map
+        mapper_update_q = f'''
+        UPDATE "{name_maps()}"."{table_names}"
+        SET "{table_names.table_name}"='{new_name}'
+        WHERE "{table_names.table_id}"='{id}';
+        '''
+        
+        full_query = full_query + mapper_update_q
+        
+        table_update_q = f'''
+        ALTER TABLE "{schema_name}"."{table_name}" 
+        RENAME TO "{new_name}";
+        '''
+        
+        full_query = full_query + table_update_q
+    
+    
+    tables_before = {key: get_table_name(int(key)) for key in rename_file}
+    queries.execute_query(full_query, connection)
+    tables_after = {key: get_table_name(int(key)) for key in rename_file}
+    
+    errors = []
+    for key in rename_file:
+        if tables_before[key] == tables_after[key]:
+            errors.append(key)
+    
+    if len(errors) != 0:
+        raise Exception(f"The following columns where not renamed correctly: {errors}")
+
+
+def rename_db_column():
+    
+    file_path = os.path.join(PATH_TO_MOUNT, "SUN-GI-metadb-test", "renamer", "db_column_renamer.json")
+    
+    # TODO: If rename file is not empty: ask user if they really want to rename
+    # TODO: If rename file is empty: error
+    
+    
+    # Load renamer file
+    with open(file_path, 'r') as file:
+        rename_file = json.load(file)
+        
+    full_query = ""
+    
+    for key in rename_file:
+        id = int(key) #  TODO: Int?
         new_name = rename_file[key]
         
         nm = name_maps()
@@ -72,7 +274,7 @@ def rename_db_column():
         mapper_update_q = f'''
         UPDATE "{name_maps()}"."{column_names}"
         SET "{column_names.column_name_db}"='{new_name}'
-        WHERE "{column_names.column_id}"={id};
+        WHERE "{column_names.column_id}"='{id}';
         '''
         
         full_query = full_query + mapper_update_q
@@ -87,9 +289,24 @@ def rename_db_column():
         
         full_query = full_query + table_update_q
 
-    
+    cols_before = {key: get_column_name(int(key)) for key in rename_file}
     queries.execute_query(full_query, connection)
+    cols_after = {key: get_column_name(int(key)) for key in rename_file}
+        
     
+    errors = []
+    for key in rename_file:
+        if cols_before[key] == cols_after[key]:
+            errors.append(key)
+    
+    if len(errors) != 0:
+        raise Exception(f"The following columns where not renamed correctly: {errors}")
+    
+   
+    
+
+# rename_db_column()
+        
     # update translater:
     # from excel name to db name
 
