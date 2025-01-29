73d390f93480c8fafc519f5c0791165d3eca9280
Magnus Johannsen <magnus.johannsen@sund.ku.dk>
2025-01-29 13:25:00 +0100

    added clean id to DB_GENERATED_COLUMNS

--- constants/db_table_related_constants.py
+++ constants/db_table_related_constants.py
@@ -119,8 +119,8 @@ class DBTableRelated:
 
     DB_GENERATED_COLUMNS = {data.top_unknown_seq_barcodes(): ['uid'],
                             data.master_depth(): ['depth_id'],
-                            data.age_depth_model(): ['depth_id']}
-        
+                            data.age_depth_model(): ['depth_id'],
+                            data.field_sample(): ['clean_id']}
 
     def check_for_duplicates():
         print(f"\nRunning tests in {str(inspect.stack()[1].code_context[0]).strip()}...\n")
